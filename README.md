52a8926dff4708c027c158c8bc44a2beea276edd
Dmitry Kripan <kripandmitry@gmail.com>
2023-08-03 01:57:10 +0300

    Calculator of the tree-digits numbers.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Figure area calculator
-This project is calculator of the area for figures.
+# Calculator of the tree-digits numbers
+This project is calculator of the tree-digits numbers.
 
 ### Used libraries
 java.util.Scanner
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,16 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please, choose one of this figures and enter the number:\n1.rectangle\n2.triangle\n3.circle");
+        System.out.println("Please, enter three-digit number:");
         Scanner scn = new Scanner(System.in);
-        int figuresNumber = scn.nextInt();
-        while (figuresNumber > 3 || figuresNumber < 1) {
-            System.out.println("Please, choose one of this figures and enter the number:\n1.rectangle\n2.triangle\n3.circle");
-            figuresNumber = scn.nextInt();
+        int number = scn.nextInt();
+        while (number > 999 || number < 100) {
+            System.out.println("Please, enter three-digit number:");
+            number = scn.nextInt();
         }
-        switch (figuresNumber) {
-            case (1) -> {
-                System.out.println("Please, enter the length of the side A of the rectangle:");
-                int a = scn.nextInt();
-                System.out.println("Please, enter the length of the side B of the rectangle:");
-                int b = scn.nextInt();
-                System.out.println("Area of the rectangle is " + a * b);
-            }
-            case (2) -> {
-                System.out.println("Please, enter the length of the side A of the triangle:");
-                int a = scn.nextInt();
-                System.out.println("Please, enter the length of the side B of the triangle:");
-                int b = scn.nextInt();
-                System.out.println("Please, enter the length of the side C of the triangle:");
-                int c = scn.nextInt();
-                System.out.println("Area of the triangle is " + 0.5 * (a + b + c));
-            }
-            case (3) -> {
-                System.out.println("Please, enter the length of the radius of the circle:");
-                int r = scn.nextInt();
-                System.out.println("Area of the circle is " + Math.PI * r * r);
-            }
-        }
+        IntStream numberStr = Integer.toString(number).chars().map(Character::getNumericValue);
+        System.out.println("The sum of digits: " + numberStr.sum());
     }
 }
