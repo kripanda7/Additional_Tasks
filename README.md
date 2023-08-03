6bec39f1999cd17f9626562471c5848eebe445bf
Dmitry Kripan <kripandmitry@gmail.com>
2023-08-03 13:14:35 +0300

    Calculator of numbers digits.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Calculator of the tree-digits numbers
-This project is calculator of the tree-digits numbers.
+This project is calculator of numbers digits.
 
 ### Used libraries
 java.util.Scanner
+java.util.stream.IntStream
 
 ### How to Run the Project
 Run in console `java Main.java`
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please, enter three-digit number:");
-        Scanner scn = new Scanner(System.in);
-        int number = scn.nextInt();
-        while (number > 999 || number < 100) {
-            System.out.println("Please, enter three-digit number:");
-            number = scn.nextInt();
+        while (true) {
+            System.out.println("Please, enter number to calculate his digits:");
+            IntStream numberStr = new Scanner(System.in)
+                    .nextLine()
+                    .chars()
+                    .filter(Character::isDigit)
+                    .map(Character::getNumericValue);
+            System.out.println("The sum of digits: " + numberStr.sum() + "\n");
         }
-        IntStream numberStr = Integer.toString(number).chars().map(Character::getNumericValue);
-        System.out.println("The sum of digits: " + numberStr.sum());
+
     }
 }
